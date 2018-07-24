7a75d7b2a48bc8a1eed5eef8292a0b55579334ff
wangkun <wangkun@autoforce.net>
2018-07-24 15:05:59 +0800

    nltk初体现

--- test_nltk/t01.py
+++ test_nltk/t01.py
@@ -1,2 +1,18 @@
 import nltk
+import jieba
 
+# 读取文件
+res = open('./a.txt').read()
+
+# 对文件内容进行分词
+words_list = jieba.lcut(res)
+print(type(words_list))
+
+# 获取文本对象
+text = nltk.text.Text(words_list)
+
+# 统计这样的词出现的次数,并显示文章内容
+# text.concordance("哈哈")
+
+# 获取该词在文本中出现的位置
+# text.common_contexts(["神"])
