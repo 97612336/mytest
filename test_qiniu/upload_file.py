941fafa67a91a326afc0f82ced09db1353c94b80
wangkun <wangkun@autoforce.net>
2018-08-21 16:55:53 +0800

    增加新修改的逻辑

--- test_qiniu/upload_file.py
+++ test_qiniu/upload_file.py
@@ -13,30 +13,39 @@ def get_keys():
     return conf_dict
 
 
-# 获取信息
-res_dict = get_keys()
-access_key = res_dict.get("ak")
-secret_key = res_dict.get("sk")
-# 利用ak和sk得到具体的用户
-q = Auth(access_key, secret_key)
+def upload_file_to_qiniu(file_path, upload_name):
+    # 获取信息
+    res_dict = get_keys()
+    access_key = res_dict.get("ak")
+    secret_key = res_dict.get("sk")
+    # 利用ak和sk得到具体的用户
+    q = Auth(access_key, secret_key)
 
-# 上传文件存储的仓库名
-bucket_name = "webfile"
-# 上传文件的用户名
-key = "a.txt"
+    # 上传文件存储的仓库名
+    bucket_name = "webfile"
+    # 上传文件的用户名
+    key = upload_name
 
-# 利用具体的用户得到具体的token
-token = q.upload_token(bucket_name)
+    # 利用具体的用户得到具体的token
+    token = q.upload_token(bucket_name)
 
-# 上传文件的地址
-localfile = "./a.txt"
+    # 上传文件的地址
+    localfile = file_path
 
-# 执行上传操作
-ret, info = put_file(token, key, localfile)
+    # 执行上传操作
+    ret, info = put_file(token, key, localfile)
 
-#判断是否长传成功
-assert ret['key'] == key
-assert ret['hash'] == etag(localfile)
+    # 判断是否长传成功
+    assert ret['key'] == key
+    assert ret['hash'] == etag(localfile)
+
+    # http://p0yb78gzi.bkt.clouddn.com/a.png
+    # 七牛云新浪邮箱
+
+    new_url = "http://p0yb78gzi.bkt.clouddn.com/" + key
+
+    return new_url
 
 
-print(info)
+res = upload_file_to_qiniu("./a.png", '123.png')
+print(res)
