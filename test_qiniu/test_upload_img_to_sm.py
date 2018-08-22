0516415c95fcf72bf5288fc8cc6c5c15230fd5a8
wangkun <wangkun@autoforce.net>
2018-08-22 09:55:07 +0800

    封装上传图片的接口

--- test_qiniu/test_upload_img_to_sm.py
+++ test_qiniu/test_upload_img_to_sm.py
@@ -1,10 +1,17 @@
+import json
+
 import requests
-import time
 
-with open("a.png", "rb") as f1:
-    data = f1.read()
-url = "http://image.baidu.com/pcdutu/a_upload?fr=html5&target=pcSearchImage&needJson=true"
 
-files = {'file': open('a.png', 'rb')}
-res = requests.post(url,files=files)
-print(res.text)
+def upload_img(img_path):
+    # url = "http://image.baidu.com/pcdutu/a_upload?fr=html5&target=pcSearchImage&needJson=true"
+    url = "https://sm.ms/api/upload"
+
+    # files = {'file': open(img_path, 'rb')}
+    files = {"smfile": open(img_path, 'rb')}
+    res = requests.post(url, files=files)
+    json_res = json.loads(res.text)
+    return json_res
+
+# res = upload_img('a.png')
+# print(res)
