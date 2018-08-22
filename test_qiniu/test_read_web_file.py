34ee2dd81f621a57b7c9e2368dad8df3d294f0a4
wangkun <wangkun@autoforce.net>
2018-08-22 15:18:48 +0800

    新增保存

--- test_qiniu/test_read_web_file.py
+++ test_qiniu/test_read_web_file.py
@@ -1,7 +1,13 @@
 import urllib.request as ur
 
+import time
+
+start_time = time.time()
 url_file = "http://p0yb78gzi.bkt.clouddn.com/a.txt"
 
 resp = ur.urlopen(url_file)
 res = resp.read()
 print(res.decode('utf-8'))
+end_time = time.time()
+cost_time = end_time - start_time
+print(cost_time)
